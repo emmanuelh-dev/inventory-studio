47041bad3556ddb97e698f0bfd88d8feabbc0b78
Omar Enrique Alvarez <oalvarez@ablsoft.com>
2022-08-23 13:58:27 -0500

    It adds config for reload the pages after changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
 module.exports = {
-    webpack: (config) => {
-      config.resolve.alias['@'] = path.resolve(__dirname);
-      config.resolve.alias['@components'] = path.resolve(__dirname, 'src/components');
-      return config;
-    }
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias['@components'] = path.resolve(__dirname, 'pages/components');
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,   // delay before rebuilding
+    };
+    return config;
+  }
 };
