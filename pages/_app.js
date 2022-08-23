27956d9a2859d2211910eaf2b2b4dfa5787b9e8c
Omar Enrique Alvarez <oalvarez@ablsoft.com>
2022-08-23 13:58:00 -0500

    It adds styles and themes. It fixes font family

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,9 @@
+import "primereact/resources/themes/arya-blue/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";                              //icons
+import "./style.css";
+ 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     return <Component className="background" {...pageProps} />
