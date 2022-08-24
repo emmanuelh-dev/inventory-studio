cdf19bc81bacc895aa6234beec10c817c5d74994
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-08-24 16:16:46 -0500

    It rename files and wrapp component in a panel

---
+++ pages/item/index.js
@@ -0,0 +1,4 @@
+import { Item } from '@components/item';
+import { withItem } from '@hocs/withItem';
+
+export default withItem(Item);
--- pages/items/index.js
+++
@@ -1,4 +0,0 @@
-import { Items } from '@components/items';
-import { withItem } from '@hocs/withItem';
-import { Panel } from 'primereact/panel';
-export default withItem(Items);
