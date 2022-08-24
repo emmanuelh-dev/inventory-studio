dff2c6cb709944a7f2cfeb2de131a7a851807328
Omar Enrique Alvarez <oalvarez@ablsoft.com>
2022-08-24 12:45:40 -0500

    it adds alias path and watch options for reald pages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    config.resolve.alias['@components'] = path.resolve(__dirname, 'pages/components');
-    config.watchOptions = {
-      poll: 1000,   // Check for changes every second
-      aggregateTimeout: 300,   // delay before rebuilding
-    };
-    return config;
-  }
+    webpack: (config) => {
+        config.resolve.alias["@"] = path.resolve(__dirname);
+        config.resolve.alias["@utils"] = path.resolve(__dirname, "src/utils");
+        config.resolve.alias["@hocs"] = path.resolve(__dirname, "src/hocs");
+        config.resolve.alias["@hooks"] = path.resolve(__dirname, "src/hooks");
+        config.resolve.alias["@constants"] = path.resolve(__dirname, "src/constants");
+        config.resolve.alias["@components"] = path.resolve(__dirname, "src/components");
+        config.watchOptions = {
+            poll: 1000, // Check for changes every second
+            aggregateTimeout: 300, // delay before rebuilding
+        };
+        return config;
+    },
 };
