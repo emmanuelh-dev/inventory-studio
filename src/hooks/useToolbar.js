0ae6d3795d339cbc5bd4117c8c3302cf7f3487d7
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-08-28 17:46:38 -0500

    it updates copy

--- src/hooks/useToolbar.js
+++ src/hooks/useToolbar.js
@@ -2,25 +2,27 @@ import { isEmpty } from '@utils';
 import { usePut } from '@hooks/usePut';
 import { usePost } from '@hooks/usePost';
 
-export const useNew = (updateState, initialState) => {
+export const useNew = (updateState, updateCopy, initialState) => {
     const onNew = () => {
         updateState(initialState);
+        updateCopy(initialState);
     };
 
     return { onNew };
 };
 
 export const useSave = (field, endpoint, showNotification) => {
-    const onSave = (state, updateState) => {
-        console.log('--->', showNotification);
+    const onSave = (state, updateState, updateCopy) => {
         if (isEmpty(state[field])) {
             usePost(endpoint.save, { ...state }).then((data) => {
                 updateState(data);
+                updateCopy(data);
                 showNotification('success');
             });
         } else {
             usePut(`${endpoint.update}/${state[field]}`, { ...state }).then((data) => {
                 updateState(data);
+                updateCopy(data);
                 showNotification('success');
             });
         }
