7344183c37c1563bd622444ef6168fef62dc1edf
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-09-06 23:17:36 -0500

    it adds function for saving with details

--- src/hooks/useToolbar.js
+++ src/hooks/useToolbar.js
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+//hooks
 import { usePut } from '@hooks/usePut';
+import { useGet } from '@hooks/useGet';
 import { usePost } from '@hooks/usePost';
-import { isEmpty, buildUrl, dateToString, stringToDate } from '@utils';
+//utils
+import { isEmpty, buildUrl, dateToString, stringToDate, isArrayEmpty } from '@utils';
 
 export const useNew = (updateState, updateCopy, initialState) => {
     const onNew = () => {
@@ -36,4 +39,47 @@ export const useSave = (field, endpoint, showNotification) => {
     };
 
     return { onSave, updateParams };
+};
+
+export const useSaveWithDetails = (fields, updateDetails, endpoint, showNotification) => {
+    let params = {};
+
+    const onSave = (state, updateState, updateCopy) => {
+        params = {
+            type: state[fields.TYPE],
+        };
+
+        if (isEmpty(state[fields.ID])) {
+            usePost(buildUrl(endpoint.save, params), dateToString({ ...state })).then((data) => {
+                fetchDetails(data, updateDetails);
+                updateState(stringToDate(data));
+                updateCopy(stringToDate(data));
+                showNotification('success');
+            });
+        } else {
+            params = {
+                type: state[fields.TYPE],
+                id: state[fields.ID],
+            };
+
+            usePut(buildUrl(endpoint.update, params), dateToString({ ...state })).then((data) => {
+                fetchDetails(data, updateDetails);
+                updateState(stringToDate(data));
+                updateCopy(stringToDate(data));
+                showNotification('success');
+            });
+        }
+    };
+
+    const fetchDetails = (state, updateDetails) => {
+        params = {
+            type: state[fields.TYPE],
+            id: state[fields.ID],
+        };
+        useGet(`${buildUrl(endpoint.details, params)}?page=0&size=10`).then((details) => {
+            updateDetails(details);
+        });
+    };
+
+    return { onSave };
 };
