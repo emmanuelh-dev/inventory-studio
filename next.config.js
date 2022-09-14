1d801238cdd5732f4ab81a67ea85cec48476030d
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-09-13 23:29:26 -0500

    it adds context to alias

--- next.config.js
+++ next.config.js
@@ -5,8 +5,10 @@ module.exports = {
         config.resolve.alias['@utils'] = path.resolve(__dirname, 'src/utils');
         config.resolve.alias['@hocs'] = path.resolve(__dirname, 'src/hocs');
         config.resolve.alias['@hooks'] = path.resolve(__dirname, 'src/hooks');
+        config.resolve.alias['@context'] = path.resolve(__dirname, 'src/context');
         config.resolve.alias['@constants'] = path.resolve(__dirname, 'src/constants');
         config.resolve.alias['@components'] = path.resolve(__dirname, 'src/components');
+
         config.watchOptions = {
             poll: 1000, // Check for changes every second
             aggregateTimeout: 300, // delay before rebuilding
