7dd4555dbfc46f643a0be553d526bc4e2d8c8662
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-09-20 23:20:59 -0500

    it removes context in configuration

--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@ const path = require('path');
 module.exports = {
     webpack: (config) => {
         config.resolve.alias['@'] = path.resolve(__dirname);
-        config.resolve.alias['@utils'] = path.resolve(__dirname, 'src/utils');
         config.resolve.alias['@hocs'] = path.resolve(__dirname, 'src/hocs');
+        config.resolve.alias['@utils'] = path.resolve(__dirname, 'src/utils');
         config.resolve.alias['@hooks'] = path.resolve(__dirname, 'src/hooks');
-        config.resolve.alias['@context'] = path.resolve(__dirname, 'src/context');
         config.resolve.alias['@constants'] = path.resolve(__dirname, 'src/constants');
         config.resolve.alias['@components'] = path.resolve(__dirname, 'src/components');
 
