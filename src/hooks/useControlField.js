4152aeef09b90bf2885ab4114f128ee8b52a787b
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-09-22 22:57:43 -0500

    refactor over useControl field It removes show notification method in validate method

--- src/hooks/useControlField.js
+++ src/hooks/useControlField.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
-export const useControlField = (fieldValue, initialValue) => {
+export const useControlField = (fieldValue, showNotification, initialValue) => {
     const [controlField, setcontrolField] = useState(initialValue);
 
-    const validate = (showNotification, fieldName) => {
-        if (!controlField == fieldValue) {
+    const validate = (fieldName) => {
+        if (controlField !== fieldValue) {
             const message = `El campo de control ${fieldName} no concide con su campo totalizado`;
             showNotification('error', message);
+            return false;
         }
+
+        return true;
     };
 
     const updateControlField = (event) => {
