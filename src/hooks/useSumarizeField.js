6ed288e995dfb91fddb66f45300a655d813f4148
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-09-22 21:11:22 -0500

    hook sumarize This only will add up documents total and quantity

--- src/hooks/useSumarizeField.js
+++ src/hooks/useSumarizeField.js
@@ -1,9 +1,17 @@
-import { useState, useEffect } from 'react';
-export const useSumarizeField = (state, updateField, field, totalField) => {
+import { useEffect } from 'react';
+export const useSumarizeField = (state, updateState, fields) => {
     useEffect(() => {
-        const sum = state[field].reduce((previousValue, element) => {
-            previousValue + element[field];
+        const totalAmount = state[fields.DETAILS].reduce((previousValue, element) => {
+            return previousValue + element[fields.TOTAL_PRICE];
         }, 0);
-        updateField(totalField, { target: { value: sum } });
-    }, [state[field]]);
+
+        const totalQuantity = state[fields.DETAILS].reduce((previousValue, element) => {
+            return previousValue + element[fields.QUANTITY];
+        }, 0);
+
+        const _state = { ...state };
+        _state[fields.TOTAL_QUANTITY] = totalQuantity;
+        _state[fields.TOTAL_AMOUNT] = totalAmount;
+        updateState(_state);
+    }, [state[fields.DETAILS]]);
 };
