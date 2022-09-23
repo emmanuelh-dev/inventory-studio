412192f929a663fd0da43fc57479fa8021d82442
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-09-22 22:28:44 -0500

    use control field show notification

--- src/hooks/useControlField.js
+++ src/hooks/useControlField.js
@@ -1,9 +1,12 @@
 import { useState } from 'react';
-export const useControlField = (field, initialValue) => {
+export const useControlField = (fieldValue, initialValue) => {
     const [controlField, setcontrolField] = useState(initialValue);
 
-    const validate = () => {
-        return controlField == field;
+    const validate = (showNotification, fieldName) => {
+        if (!controlField == fieldValue) {
+            const message = `El campo de control ${fieldName} no concide con su campo totalizado`;
+            showNotification('error', message);
+        }
     };
 
     const updateControlField = (event) => {
