de123faa79072aaf0d370624d0e8268cd5d88f58
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-09-24 14:33:35 -0500

    it adds up only details not marked as deleted

--- src/hooks/useSumarizeField.js
+++ src/hooks/useSumarizeField.js
@@ -2,10 +2,16 @@ import { useEffect } from 'react';
 export const useSumarizeField = (state, updateState, fields) => {
     useEffect(() => {
         const totalAmount = state[fields.DETAILS].reduce((previousValue, element) => {
+            if (element[fields.DELETED]) {
+                return previousValue + 0;
+            }
             return previousValue + element[fields.TOTAL_PRICE];
         }, 0);
 
         const totalQuantity = state[fields.DETAILS].reduce((previousValue, element) => {
+            if (element[fields.DELETED]) {
+                return previousValue + 0;
+            }
             return previousValue + element[fields.QUANTITY];
         }, 0);
 
