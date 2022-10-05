e200fe3343f948607ebb24208ccb4718a9a0f983
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-10-04 22:34:21 -0500

    it adds crud functionality for details. It removes old version of crud, it show dialog window now

--- src/components/details/index.jsx
+++ src/components/details/index.jsx
@@ -1,22 +1,28 @@
+import React, { useState } from 'react';
 //constants
-import { getValue } from '@utils';
+import { isArrayEmpty } from '@utils';
+import { detailState } from '@constants';
+import { detailsToolbar } from '@constants/options';
+import { useFormState } from '@hooks/useFormState';
 //components
 import { Panel } from 'primereact/panel';
+import { Button } from 'primereact/button';
 import { Column } from 'primereact/column';
 import { Menubar } from 'primereact/menubar';
 import { DataTable } from 'primereact/datatable';
 import { FilterMatchMode } from 'primereact/api';
+//customs components
+import { DetailDialog } from '@components/detaildialog';
 
 export const Details = (props) => {
+    const [visible, setVisible] = useState(false);
     const {
-        data,
-        fields,
-        columns,
-        selection,
-        updateSelection,
-        detailToolbar,
-        updateDocumentField,
-    } = {
+        state: detail,
+        updateState: updateDetail,
+        updateField: updateDetailField,
+    } = useFormState({}, { ...detailState });
+
+    const { data, fields, columns, removeDetail, selection, updateDetails, updateSelection } = {
         ...props,
     };
 
@@ -24,32 +30,10 @@ export const Details = (props) => {
         deleted: { value: false, matchMode: FilterMatchMode.EQUALS },
     };
 
-    const updateField = (element, field, event) => {
-        element[field] = getValue(event);
-        element = updateTotal(element, field);
-        updateContent(element);
-    };
-
-    const updateTotal = (element, field) => {
-        if (field == fields.UNIT_PRICE || field == fields.QUANTITY) {
-            element[fields.TOTAL_PRICE] = element[fields.UNIT_PRICE] * element[fields.QUANTITY];
-            return element;
-        }
-        return element;
-    };
-
-    const updateContent = (element) => {
-        const _data = [...data];
-
-        const index = _data.findIndex((detail) => {
-            return detail[fields.LINE_NUMBER] == element[fields.LINE_NUMBER];
-        });
-
-        if (index > -1) {
-            _data[index] = element;
-
-            updateDocumentField(fields.DETAILS, { target: { value: _data } });
-        }
+    const addDetail = () => {
+        const _detail = { ...detail };
+        updateDetails(_detail);
+        hideDialog();
     };
 
     const selectionChange = (event) => {
@@ -57,58 +41,88 @@ export const Details = (props) => {
     };
 
     const toolbar = () => {
+        console.log('---->', isArrayEmpty(data));
+        const onRemoveDetail = {
+            command: removeDetail,
+            state: isArrayEmpty(data),
+        };
+        const detailToolbar = createDetailToolbar(showDialog, onRemoveDetail);
         return <Menubar model={detailToolbar} />;
     };
 
+    const actionTemplate = (rowData) => {
+        const editDetail = () => {
+            updateDetail(rowData);
+            showDialog();
+        };
+
+        return (
+            <Button
+                icon="pi pi-pencil"
+                onClick={editDetail}
+                className="p-button-rounded p-button-success mr-2"
+            />
+        );
+    };
+
+    //dialog
+    const showDialog = () => {
+        setVisible(true);
+    };
+    const hideDialog = () => {
+        updateDetail({ ...detailState });
+        setVisible(false);
+    };
+
+    const detailDialogProps = {
+        fields,
+        visible,
+        addDetail,
+        hideDialog,
+        rowData: detail,
+        updateField: updateDetailField,
+    };
+
     return (
-        <Panel header={toolbar}>
+        <Panel header={toolbar()}>
             <DataTable
                 rows={10}
                 paginator
                 value={data}
-                editMode="cell"
                 filters={filters}
-                dataKey="lineNumber"
                 selection={selection}
                 selectionMode="checkbox"
                 onSelectionChange={selectionChange}
             >
                 <Column selectionMode="multiple" className="col-1" />
                 {columns.map((element) => {
-                    return cell(element, updateField, 'lineNumber');
+                    return createColumn(element, 'lineNumber');
                 })}
+                <Column body={actionTemplate} />
             </DataTable>
+            <DetailDialog {...detailDialogProps} className="col-1" />
         </Panel>
     );
 };
 
-const cell = (column, updateField, key) => {
-    if (column.editor !== undefined) {
-        return (
-            <Column
-                key={key}
-                className="col-2"
-                field={column.field}
-                header={column.header}
-                editor={(options) => {
-                    return column.editor(options, updateField);
-                }}
-                body={(row) => {
-                    return column.body(row, column.field);
-                }}
-            />
-        );
-    } else {
-        return (
-            <Column
-                key={key}
-                className="col-2"
-                field={column.field}
-                header={column.header}
-                body={(row) => {
-                    return column.body(row, column.field);
-                }}
-            />
-        );
-    }
+const createDetailToolbar = (onAdd, onRemove) => {
+    const _detailsToolbar = [...detailsToolbar];
+    _detailsToolbar[0].command = onAdd;
+    _detailsToolbar[1].command = onRemove.command;
+    _detailsToolbar[1].disabled = onRemove.state;
+    return _detailsToolbar;
+};
+
+const createColumn = (column, key) => {
+    return (
+        <Column
+            key={key}
+            className="col-2"
+            field={column.field}
+            header={column.header}
+            body={(row) => {
+                return column.body(row, column.field);
+            }}
+        />
+    );
 };
