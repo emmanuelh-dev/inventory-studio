1bb9fe8097555611e6812fb010072a7789b187fb
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-10-16 15:46:52 -0500

    it adds page list for input document

---
+++ src/hocs/withInputReceptionList.js
@@ -0,0 +1,17 @@
+import { documentSearchFields, documentFilters } from '@constants/options';
+
+export const withInputReceptionList = (WrappedComponent) => (props) => {
+    const endpoint = {
+        redirect: 'reception',
+        search: process.env.NEXT_PUBLIC_RECEPTIONS_SEARCH,
+        suggestions: `${process.env.NEXT_PUBLIC_RECEPTIONS_SUGGESTIONS}INPUT`,
+    };
+
+    return (
+        <WrappedComponent
+            endpoint={endpoint}
+            filters={documentFilters}
+            fields={documentSearchFields}
+        />
+    );
+};
