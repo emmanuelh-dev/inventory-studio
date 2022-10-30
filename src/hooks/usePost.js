ac674894b3ccb2bd7910a7b62dd711708e7f82cd
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-10-30 16:03:01 -0600

    it adds functionality for handling error

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -3,6 +3,10 @@ import { buildFetchOptions } from '@utils/request';
 export const usePost = async (url, body) => {
     const options = buildFetchOptions('POST', body);
     const response = await fetch(url, options);
+    if (!response.ok) {
+        const body = await response.json();
+        throw new Error(body.message);
+    }
     const data = await response.json();
     return data;
 };
