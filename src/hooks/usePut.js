f2aa6244a1cd774aaf31d7b8f52fab481142118c
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-10-30 16:01:55 -0600

    it adds validation to get error

--- src/hooks/usePut.js
+++ src/hooks/usePut.js
@@ -3,6 +3,10 @@ import { buildFetchOptions } from '@utils/request';
 export const usePut = async (url, body) => {
     const options = buildFetchOptions('PUT', body);
     const response = await fetch(url, options);
+    if (!response.ok) {
+        const body = await response.json();
+        throw new Error(body.message);
+    }
     const data = await response.json();
     return data;
 };
