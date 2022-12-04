29d6b0b9adb28b3931db8c3db9a3fed7f95486d9
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-12-04 01:27:15 -0600

    refactor: It can download files

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -7,6 +7,12 @@ export const usePost = async (url, body) => {
         const body = await response.json();
         throw new Error(body.message);
     }
-    const data = await response.json();
+
+    let data;
+    if (url.includes('reports')) {
+        data = await response.arrayBuffer();
+    } else {
+        data = await response.json();
+    }
     return data;
 };
