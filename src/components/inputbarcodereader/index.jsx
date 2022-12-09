c274502ae981a610bea09b88707d53c94460cc3e
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-12-09 01:00:39 -0600

    it adds input barcode reader component

---
+++ src/components/inputbarcodereader/index.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { useGet } from '@hooks/useGet';
+import { getValue, replaceParams } from '@utils';
+import { InputText } from 'primereact/inputtext';
+
+export const InputBarcodeReader = (props) => {
+    const [barcode, setBarcode] = useState('');
+    const { processBarcode, documentType } = { ...props };
+    const endpoint = {
+        readBarcode: process.env.NEXT_PUBLIC_DISPATCHES_READ_BARCODE,
+    };
+
+    const readBarcode = (value) => {
+        const params = {
+            type: documentType,
+            barcode: value,
+            format: 'CODE128',
+        };
+        const url = replaceParams(endpoint.readBarcode, params);
+        useGet(url).then((data) => {
+            processBarcode(data);
+        });
+    };
+
+    const onChange = (event) => {
+        const value = getValue(event);
+        setBarcode(value);
+    };
+
+    const onKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            console.log('---->', barcode);
+            readBarcode(barcode);
+        }
+    };
+
+    return <InputText value={barcode} onChange={onChange} onKeyDown={onKeyDown} />;
+};
