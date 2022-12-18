76173178e8180cef82b7d662fbf5147997f9344e
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2022-12-18 16:18:57 -0600

    it adds handling of error in get request.

--- src/hooks/useGet.js
+++ src/hooks/useGet.js
@@ -1,5 +1,11 @@
 export const useGet = async (url) => {
     const response = await fetch(url);
+
+    if (!response.ok) {
+        const body = await response.json();
+        throw new Error(body.message);
+    }
+
     const data = await response.json();
     return data;
 };
