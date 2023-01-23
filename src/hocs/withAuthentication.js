22221178df9a26657c56f8991f989aedd57a9110
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-01-23 16:59:00 -0600

    it adds basic function for authentication hoc

---
+++ src/hocs/withAuthentication.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+export const withAuthentication = (WrappedComponent) => (props) => {
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: '',
+    });
+
+    const handleInputChange = (event) => {
+        setCredentials({
+            ...credentials,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    const authenticationProps = {
+        credentials,
+        handleFormSubmit,
+        handleInputChange,
+    };
+
+    return <WrappedComponent {...authenticationProps} />;
+};
