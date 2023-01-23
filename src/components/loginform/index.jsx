2aba07ab4dee97412af6b1c9c5ff65ccb1544af9
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-01-23 16:17:03 -0600

    it adds basic ui login form

--- src/components/loginform/index.jsx
+++ src/components/loginform/index.jsx
@@ -1,7 +1,21 @@
 import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
 
-export const LoginForm = (props)=>{
-    return(<Card>
-        
-    </Card>);
-}
+export const LoginForm = (props) => {
+    return (
+        <Card className="p-fluid formgrid grid">
+            <div className="field col-12">
+                <label>Nombre de usuario</label>
+                <InputText type="email" className="p-inputtext-lg" value="" />
+            </div>
+            <div className="field col-12">
+                <label>Contraseña</label>
+                <InputText type="password" className="p-inputtext-lg" value="" />
+            </div>
+            <div className="field col-12">
+                <Button type="submit" label="Enviar" className="p-button-lg " />
+            </div>
+        </Card>
+    );
+};
