6fb5c6e9f0857120d71843154acbfa46f7ad2047
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-01-23 16:59:36 -0600

    it adds form functionaluty and props

--- src/components/loginform/index.jsx
+++ src/components/loginform/index.jsx
@@ -3,19 +3,34 @@ import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 
 export const LoginForm = (props) => {
+    const { credentials, handleInputChange, handleFormSubmit } = { ...props };
     return (
         <Card className="p-fluid formgrid grid">
-            <div className="field col-12">
-                <label>Nombre de usuario</label>
-                <InputText type="email" className="p-inputtext-lg" value="" />
-            </div>
-            <div className="field col-12">
-                <label>Contraseña</label>
-                <InputText type="password" className="p-inputtext-lg" value="" />
-            </div>
-            <div className="field col-12">
-                <Button type="submit" label="Enviar" className="p-button" />
-            </div>
+            <form onSubmit={handleFormSubmit}>
+                <div className="field col-12">
+                    <label>Nombre de usuario</label>
+                    <InputText
+                        type="email"
+                        name="username"
+                        className="p-inputtext-lg"
+                        value={credentials.username}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div className="field col-12">
+                    <label>Contraseña</label>
+                    <InputText
+                        type="password"
+                        name="password"
+                        className="p-inputtext-lg"
+                        value={credentials.password}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div className="field col-12">
+                    <Button type="submit" label="Enviar" className="p-button" />
+                </div>
+            </form>
         </Card>
     );
 };
