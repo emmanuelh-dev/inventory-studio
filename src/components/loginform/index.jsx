a7d3c662139643121ff2735e50d12cba16d48499
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-01-24 13:52:38 -0600

    it fixes containers

--- src/components/loginform/index.jsx
+++ src/components/loginform/index.jsx
@@ -5,32 +5,34 @@ import { InputText } from 'primereact/inputtext';
 export const LoginForm = (props) => {
     const { credentials, handleInputChange, handleFormSubmit } = { ...props };
     return (
-        <Card className="p-fluid formgrid grid">
-            <form onSubmit={handleFormSubmit}>
-                <div className="field col-12">
-                    <label>Nombre de usuario</label>
-                    <InputText
-                        type="email"
-                        name="username"
-                        className="p-inputtext-lg"
-                        value={credentials.username}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div className="field col-12">
-                    <label>Contraseña</label>
-                    <InputText
-                        type="password"
-                        name="password"
-                        className="p-inputtext-lg"
-                        value={credentials.password}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div className="field col-12">
-                    <Button type="submit" label="Enviar" className="p-button" />
-                </div>
-            </form>
-        </Card>
+        <div className="flex align-content-center justify-content-center flex-wrap login-container">
+            <Card className="login-card">
+                <form onSubmit={handleFormSubmit} className="p-fluid formgrid grid">
+                    <div className="field col-12">
+                        <label>Nombre de usuario</label>
+                        <InputText
+                            type="email"
+                            name="username"
+                            className="p-inputtext-lg"
+                            value={credentials.username}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div className="field col-12">
+                        <label>Contraseña</label>
+                        <InputText
+                            type="password"
+                            name="password"
+                            className="p-inputtext-lg"
+                            value={credentials.password}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div className="field col-12">
+                        <Button type="submit" label="Enviar" className="p-button" />
+                    </div>
+                </form>
+            </Card>
+        </div>
     );
 };
