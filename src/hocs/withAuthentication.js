d37582828bbc1983e6c9ab611881a9ed6b8b670a
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-01-24 13:52:53 -0600

    it removes import nor required

--- src/hocs/withAuthentication.js
+++ src/hocs/withAuthentication.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 export const withAuthentication = (WrappedComponent) => (props) => {
     const [credentials, setCredentials] = useState({
         username: '',
