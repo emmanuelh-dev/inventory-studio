9d65878bac56647322fe12cb171c6de8c9b1456c
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-01-27 11:35:49 -0600

    format

--- src/utils/request.js
+++ src/utils/request.js
@@ -21,10 +21,10 @@ export const buildAuthOptions = (credentials, data) => {
     const { client, secret } = { ...credentials };
     const { username, password } = { ...data };
     const options = {
-        method: 'POST',
-        redirect: 'manual',
         mode: 'cors',
+        method: 'POST',
         cache: 'default',
+        redirect: 'manual',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             Authorization: `Basic ${Buffer.from(`${client}:${secret}`).toString('base64')}`,
