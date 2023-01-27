5f8904d2c36b3575246dce486b83323c37d48e91
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-01-26 23:41:47 -0600

    it adds auth build options. it adds functionality for getting the token

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,3 +16,26 @@ export const buildFetchOptions = (method, data) => {
 
     return options;
 };
+
+export const buildAuthOptions = (credentials, data) => {
+    const { client, secret } = { ...credentials };
+    const { username, password } = { ...data };
+    const options = {
+        method: 'POST',
+        redirect: 'manual',
+        mode: 'cors',
+        cache: 'default',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            Authorization: `Basic ${Buffer.from(`${client}:${secret}`).toString('base64')}`,
+        },
+        body: new URLSearchParams({
+            username: username,
+            password: password,
+            grant_type: 'password',
+            scopes: 'read write',
+        }),
+    };
+
+    return options;
+};
