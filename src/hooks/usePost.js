5b0173bad86f72a912864a69144248a26e665c5c
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-01-26 23:42:04 -0600

    it adds function for getting the token

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -1,4 +1,4 @@
-import { buildFetchOptions } from '@utils/request';
+import { buildFetchOptions, buildAuthOptions } from '@utils/request';
 
 export const usePost = async (url, body) => {
     const options = buildFetchOptions('POST', body);
@@ -16,3 +16,20 @@ export const usePost = async (url, body) => {
     }
     return data;
 };
+
+export const useAuthPost = async (url, credentials, body) => {
+    const options = buildAuthOptions(credentials, body);
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        const body = await response.json();
+        throw new Error(body.message);
+    }
+
+    let data;
+    if (url.includes('reports')) {
+        data = await response.arrayBuffer();
+    } else {
+        data = await response.json();
+    }
+    return data;
+}
