1a8f403f975693fcb28efef98b1c15e7656f8dda
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-01-28 00:19:06 -0600

    it adds cookie

--- src/hocs/withAuthentication.js
+++ src/hocs/withAuthentication.js
@@ -3,12 +3,12 @@ import { useAuthPost } from '@hooks/usePost';
 export const withAuthentication = (WrappedComponent) => (props) => {
     const endpoint = {
         auth: process.env.NEXT_PUBLIC_AUTH,
-    }
+    };
 
     const credentials = {
         client: process.env.NEXT_PUBLIC_CLIENT,
-        secret: process.env.NEXT_PUBLIC_SECRET
-    }
+        secret: process.env.NEXT_PUBLIC_SECRET,
+    };
 
     const [userAccount, setUserAccount] = useState({
         username: '',
@@ -25,7 +25,9 @@ export const withAuthentication = (WrappedComponent) => (props) => {
     const handleFormSubmit = async (event) => {
         event.preventDefault();
         const response = await useAuthPost(endpoint.auth, credentials, userAccount);
-        console.log('token json: ', response);
+        const { access_token, token_type, refresh_token, expires_in, scope, jti } = { ...response };
+        document.cookie = `access_token=${access_token};`;
+        document.cookie = `refresh_token=${refresh_token}`;
     };
 
     const authenticationProps = {
