a3951b911703f46be39be147478010e5480cd88c
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-02-18 18:21:41 -0600

    it changes menu option name

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -11,7 +11,7 @@ export const Navbar = (props) => {
             label: 'Documentos',
             items: [
                 {
-                    label: 'Recepciones',
+                    label: 'Ingreso',
                     url: '/reception',
                 },
                 {
