a24b8491a8ddb0bfae5ca144ca9a26c1fd929a2f
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-02-19 13:53:27 -0600

    it validates json web token. IT check if jwt is valid or if it exist otherwise it will redirect to login page.

--- middleware.js
+++ middleware.js
@@ -4,23 +4,26 @@ const middleware = async (request) => {
     const endpoint = {
         verifyOAuth: process.env.NEXT_PUBLIC_AUTH_VERIFY,
     };
-
-    const jwt = request.cookies.get('access_token');
-    if (jwt && jwt !== undefined) {
-        const url = `${endpoint.verifyOAuth}${new URLSearchParams({ token: jwt })}`;
-        const valid = validateToken(url);
+    const pathname = request.nextUrl.pathname;
+    if (!(pathname.includes('_next') || pathname.includes('favicon') || pathname.includes("login"))) {
+        const jwt = request.cookies.get('access_token');
+        if (jwt && jwt !== undefined) {
+            const url = `${endpoint.verifyOAuth}${new URLSearchParams({ token: jwt })}`;
+            const valid = validateToken(url);
+        } else {
+            return NextResponse.redirect(new URL('/login', request.url));
+        }
     }
-    NextResponse.next();
+    return NextResponse.next();
 };
 
-const validateToken = async (url)=>{
-    try{
+const validateToken = async (url) => {
+    try {
         const response = await useGet(url);
         return response.active;
-    }catch(error){
-        
+    } catch (error) {
         console.log('mensaje de error ', error);
     }
-}
+};
 
 export default middleware;
