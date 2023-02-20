647ced561e778ecb7073fcb31f539778ee2bf05d
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-02-20 00:07:42 -0600

    it adds configuration of next auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,63 @@
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
+//hooks
+import { useAuthPost } from '@hooks/usePost';
+export const authOptions = {
+    secret: process.env.JWT_SECRET,
+    session: {
+        strategy: 'jwt',
+    },
+    providers: [
+        CredentialsProvider({
+            type: 'credentials',
+            credentials: {},
+            async authorize(userAccount, req) {
+                const { username, password } = { ...userAccount };
+                const account = { username, password };
+                const credentials = {
+                    client: process.env.JWT_CLIENT,
+                    secret: process.env.JWT_SECRET,
+                };
+
+                try {
+                    const response = await useAuthPost(
+                        process.env.AUTHENTICATION,
+                        credentials,
+                        account
+                    );
+                    console.log('response --->', response);
+                    return response;
+                } catch (error) {
+                    console.log('error --->', error);
+                    return null;
+                }
+            },
+        }),
+    ],
+    pages: {
+        signIn: '/login',
+    },
+    callbacks: {
+        async jwt({ token, user, account, profile }) {
+            console.log('::::::::>', token);
+            if (user) {
+                token.accessToken = user.access_token;
+                token.refreshToken = user.refresh_token;
+                token.refreshToken = user.refresh_token;
+                token.accessTokenExpires = user.expires_in;
+            }
+            console.log('---===:::>', token);
+            return token;
+        },
+        async session({ token, session }) {
+            session.user.accessToken = token.accessToken;
+            session.user.refreshToken = token.refreshToken;
+            session.user.accessTokenExpires = token.accessTokenExpires;
+
+            console.log('--------->', token);
+            return session;
+        },
+    },
+};
+
+export default NextAuth(authOptions);
