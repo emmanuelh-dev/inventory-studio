93e6551f126c35bf356e2393915fbe1c776c8096
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-03-03 13:12:50 -0600

    it adds  next-auth middleware to protect pages.

--- middleware.js
+++ middleware.js
@@ -1,29 +1,18 @@
-// import { NextResponse } from 'next/server';
-// import { useGet } from '@hooks/useGet';
-// const middleware = async (request) => {
-//     const endpoint = {
-//         verifyOAuth: process.env.NEXT_PUBLIC_AUTH_VERIFY,
-//     };
-//     const pathname = request.nextUrl.pathname;
-//     if (!(pathname.includes('_next') || pathname.includes('favicon') || pathname.includes("login"))) {
-//         const jwt = request.cookies.get('access_token');
-//         if (jwt && jwt !== undefined) {
-//             const url = `${endpoint.verifyOAuth}${new URLSearchParams({ token: jwt })}`;
-//             const valid = validateToken(url);
-//         } else {
-//             return NextResponse.redirect(new URL('/login', request.url));
-//         }
-//     }
-//     return NextResponse.next();
-// };
-
-// const validateToken = async (url) => {
-//     try {
-//         const response = await useGet(url);
-//         return response.active;
-//     } catch (error) {
-//         console.log('mensaje de error ', error);
-//     }
-// };
-const middleware = async (request) =>{}
-export default middleware;
+import { withAuth } from 'next-auth/middleware';
+const middleware = async (request) => {};
+export default withAuth(middleware);
+export const config = {
+    matcher: [
+        '/item',
+        '/dispatch',
+        '/reception',
+        '/inventory',
+        '/item-list',
+        '/warehouse',
+        '/warehouse-list',
+        '/input-reception-list',
+        '/output-dispatch-list',
+        '/sales-return-reception-list',
+        '/purchase-return-dispatch-list',
+    ],
+};
