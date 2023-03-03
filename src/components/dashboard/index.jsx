d9ce6d271c33ce0793cf9ad4684d5df7022b46a3
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-03-03 12:29:38 -0600

    it secures dashboard

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,5 +1,25 @@
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+//custom components
 import { Navbar } from '@components/navbar';
+//components
+import { ProgressSpinner } from 'primereact/progressspinner';
 export const Dashboard = ({ children }) => {
+    const router = useRouter();
+    const { status } = useSession();
+    if (status === 'loading') {
+        return (
+            <div className="flex justify-content-center flex-wrap">
+                <ProgressSpinner animationDuration=".5s" />
+            </div>
+        );
+    }
+
+    if (status === 'unauthenticated') {
+        router.push('/login');
+        return;
+    }
+
     return (
         <div className="grid">
             <div className="col-2">
