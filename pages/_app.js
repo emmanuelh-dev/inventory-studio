8b6ad3ad9e676824757aa8e44ebe52c324a4f1ec
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-03-03 12:29:14 -0600

    it adds session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
-import "primereact/resources/themes/arya-blue/theme.css";  //theme
-import "primereact/resources/primereact.min.css";                  //core css
-import "primeicons/primeicons.css";
-import "primeflex/primeflex.css";                              //icons
-import "./style.css";
- 
+import { SessionProvider } from 'next-auth/react';
+import 'primereact/resources/themes/arya-blue/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css'; //icons
+import './style.css';
+
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-    return <Component className="background" {...pageProps} />
-  }
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+    return (
+        <SessionProvider session={session}>
+            <Component className="background" {...pageProps} />
+        </SessionProvider>
+    );
+}
