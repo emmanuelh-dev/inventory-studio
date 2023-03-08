dfbbca6e01c2a37cd6256d117fe88777b8fceea4
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-03-07 23:11:41 -0600

    format

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -41,7 +41,7 @@ export const authOptions = {
             }
             return token;
         },
-        async session({ token, session }) {
+        async session({ session, token }) {
             session.user.accessToken = token.accessToken;
             session.user.refreshToken = token.refreshToken;
             session.user.accessTokenExpires = token.accessTokenExpires;
