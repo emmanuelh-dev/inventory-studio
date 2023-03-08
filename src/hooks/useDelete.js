4458a52b67539181c817c8e89d0f43a26a1e5402
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-03-07 23:12:49 -0600

    it adds useDelete for signingout

--- src/hooks/useDelete.js
+++ src/hooks/useDelete.js
@@ -1,7 +1,23 @@
 import { buildFetchOptions } from '@utils/request';
-
 export const useDelete = async (url) => {
     const options = buildFetchOptions('DELETE', {});
     const response = await fetch(url, options);
     return response.status;
+};
+
+export const useAuthDelete = async (url, session) => {
+    const options = {
+        method: 'DELETE',
+        redirect: 'manual',
+        mode: 'cors',
+        cache: 'default',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            //Authorization: `Bearer ${Buffer.from(session.user.accessToken).toString('base64')}`,
+            Authorization: `Bearer ${session.user.accessToken}`,
+        },
+    };
+
+    const response = await fetch(url, options);
+    return response;
 };
