e6c746b2c338efba0ce8ed0105d559676f1b35a1
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-03-07 23:12:34 -0600

    it adds signout functionallity

--- src/components/userbar/index.jsx
+++ src/components/userbar/index.jsx
@@ -1,17 +1,27 @@
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
+//components
 import { Button } from 'primereact/button';
+import { Menubar } from 'primereact/menubar';
+//hooks
+import { useAuthDelete } from '@hooks/useDelete';
 export const Userbar = (props) => {
-    const handleSignoutClick = (event) => {};
-
-    return (
-        <>
-            <Button
-                label="Cerrar sesion"
-                iconPos="left"
-                icon="pi pi-sign-out"
-                className="p-button-raised p-button-text p-button-plain"
-                onClick={handleSignoutClick}
-            />
-        </>
+    const { data: session } = useSession();
+    const handleSignoutClick = async (event) => {
+        event.preventDefault();
+        const response = await useAuthDelete(process.env.NEXT_PUBLIC_SIGNOUT, session);
+        if (response.ok) {
+            signOut({ callbackUrl: '/login' });
+        }
+    };
+    const RightElement = () => (
+        <Button
+            label="Cerrar sesion"
+            iconPos="left"
+            icon="pi pi-sign-out"
+            className="p-button-raised p-button-text p-button-plain"
+            onClick={handleSignoutClick}
+        />
     );
+
+    return <Menubar model={[]} end={<RightElement />} />;
 };
