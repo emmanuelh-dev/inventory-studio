b3e148c517258b08ab0182db68347b57016d0f31
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-05-14 12:28:41 -0600

    it adds session to put function

--- src/hooks/usePut.js
+++ src/hooks/usePut.js
@@ -1,7 +1,7 @@
 import { buildFetchOptions } from '@utils/request';
 
-export const usePut = async (url, body) => {
-    const options = buildFetchOptions('PUT', body);
+export const usePut = async (url, session, body) => {
+    const options = buildFetchOptions('PUT', session, body);
     const response = await fetch(url, options);
     if (!response.ok) {
         const body = await response.json();
