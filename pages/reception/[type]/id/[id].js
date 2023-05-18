cd5450001dc2aedc6a273ddea1218c4ce64a279a
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-05-17 23:35:59 -0600

    it implements api services

--- pages/reception/[type]/id/[id].js
+++ pages/reception/[type]/id/[id].js
@@ -1,22 +1,17 @@
 import { useRouter } from 'next/router';
 import React, { useState, useEffect } from 'react';
-//utils
-import { stringToDate } from '@utils';
-//hooks
-import { useGet } from '@hooks/useGet';
-//custom components
-import { MessageDialog } from '@components/messagedialog';
-//components
-import { ProgressSpinner } from 'primereact/progressspinner';
-//hocs
+import services from '@services/api-services';
 import { withReception } from '@hocs/withReception';
+import { MessageDialog } from '@components/messagedialog';
+import { ProgressSpinner } from 'primereact/progressspinner';
+
 const Reception = withReception;
+
 export default () => {
     const [showDialog, setShowDialog] = useState(false);
     const [document, setDocument] = useState(null);
     const router = useRouter();
     const { type, id } = router.query;
-    const endpoint = process.env.NEXT_PUBLIC_RECEPTIONS_SUGGESTIONS;
     const messageDialogProps = {
         id,
         type,
@@ -25,26 +20,28 @@ export default () => {
             setShowDialog(false);
             router.push(`/reception`);
         },
-        
+    };
+
+    const fetchData = async () => {
+        if (type && id) {
+            try {
+                const result = await services.findReceptionDocumentById(type, id);
+                setDocument(result);
+            } catch (error) {
+                setShowDialog(true);
+            }
+        }
     };
 
     useEffect(() => {
-        if (type !== undefined && id !== undefined) {
-            useGet(`${endpoint}${type}/id/${id}`).then((data) => {
-                const _document = stringToDate(data);
-                setDocument(_document);
-            })
-            .catch((error) => {
-                setShowDialog(true);
-            });;
-        }
+        fetchData();
     }, [type, id]);
 
     if (document == null) {
         return (
             <div className="flex justify-content-center flex-wrap">
                 <ProgressSpinner animationDuration=".5s" />
-                <MessageDialog {...messageDialogProps}/>
+                <MessageDialog {...messageDialogProps} />
             </div>
         );
     }
