bf7e2e2f001d57af285e32e85fb8764b253c2b12
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-05-19 23:29:54 -0600

    it uses messages types

--- src/hooks/useControlField.js
+++ src/hooks/useControlField.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { MESSAGE_TYPES } from '@constants';
 export const useControlField = (fieldValue, showNotification, initialValue) => {
     const [controlField, setcontrolField] = useState(initialValue);
 
@@ -6,7 +7,7 @@ export const useControlField = (fieldValue, showNotification, initialValue) => {
         const validation = controlField !== fieldValue;
         if (validation) {
             const message = `El campo de control ${fieldName} no concide con su campo totalizado`;
-            showNotification('error', message);
+            showNotification(MESSAGE_TYPES.ERROR, message);
         }
         return !validation;
     };
