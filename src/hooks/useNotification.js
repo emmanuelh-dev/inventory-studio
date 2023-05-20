47d18d32b2f14464bc89978270b2b40ccfbaeeff
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-05-19 22:34:15 -0600

    It moves types to constants folder

--- src/hooks/useNotification.js
+++ src/hooks/useNotification.js
@@ -4,28 +4,12 @@ export const useNotification = () => {
     const notification = useRef(null);
 
     const showNotification = (type, message) => {
-        const param = types[type];
         if (message) {
-            param.detail = message;
+            type.detail = message;
         }
 
-        notification.current.show(param);
+        notification.current.show(type);
     };
 
     return { notification, showNotification };
-};
-
-const types = {
-    success: {
-        severity: 'success',
-        summary: 'Existoso',
-        detail: 'Registro creado con exito',
-        life: 3000,
-    },
-    error: {
-        severity: 'error',
-        summary: 'Error',
-        detail: 'El registro no fue guardado',
-        life: 3000,
-    },
 };
