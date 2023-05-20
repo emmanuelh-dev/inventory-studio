83c13d05e7105af273c73c244b56a6acde0ecaa5
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-05-19 22:10:49 -0600

    it adds messages alias

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
         config.resolve.alias['@hocs'] = path.resolve(__dirname, 'src/hocs');
         config.resolve.alias['@utils'] = path.resolve(__dirname, 'src/utils');
         config.resolve.alias['@hooks'] = path.resolve(__dirname, 'src/hooks');
+        config.resolve.alias['@messages'] = path.resolve(__dirname, 'src/messages');
         config.resolve.alias['@services'] = path.resolve(__dirname, 'src/services');
         config.resolve.alias['@constants'] = path.resolve(__dirname, 'src/constants');
         config.resolve.alias['@components'] = path.resolve(__dirname, 'src/components');
