ca2db6a2a89a0c5d1270dec7a9f8dcee0466ca2a
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-05-19 22:35:21 -0600

    it sorts messages

--- src/messages/index.js
+++ src/messages/index.js
@@ -1,7 +1,7 @@
 export const MESSAGES = {
     ERROR_RECORD_CREATED: 'El registro no fue guardado',
     SUCCESS_RECORD_CREATED: 'El registro creado con exito',
-    SUCCESS_RECORD_UPDATED: 'El registro fue actualizado con exito',
     SUCESS_RECORD_DELETED: 'El registro fue eliminado con exito',
     SUCESS_RECORD_RELEASED: 'El registro fue liberado con exito',
+    SUCCESS_RECORD_UPDATED: 'El registro fue actualizado con exito',
 };
