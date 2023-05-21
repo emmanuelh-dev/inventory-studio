7fb728aa0e3437c1281ebd3c79e5f6197d42595c
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-05-21 15:10:35 -0600

    it implements services

--- src/hocs/withItemList.js
+++ src/hocs/withItemList.js
@@ -1,10 +1,30 @@
+import services from '@services/api-services';
 import { itemSearchFields, itemFilters } from '@constants/options';
 
 export const withItemList = (WrappedComponent) => (props) => {
-    const endpoint = {
-        redirect: 'item',
-        search: process.env.NEXT_PUBLIC_ITEMS_SEARCH,
-        suggestions: process.env.NEXT_PUBLIC_ITEMS_SUGGESTIONS,
+    const getDataAsPage = async () => {
+        const result = await services.findAllItemsAsPage();
+        return result;
     };
-    return <WrappedComponent endpoint={endpoint} fields={itemSearchFields} filters={itemFilters} />;
+
+    const getDataByPage = async (page) => {
+        const result = await services.findAllItemsByPage(page);
+        return result;
+    };
+
+    const getDataByFilter = async (filter) => {
+        const result = await services.findItemByFilter(filter);
+        return result;
+    };
+
+    const listProps = {
+        redirect: 'item/',
+        filters: itemFilters,
+        fields: itemSearchFields,
+        getDataAsPage: getDataAsPage,
+        getDataByPage: getDataByPage,
+        getDataByFilter: getDataByFilter,
+    };
+
+    return <WrappedComponent {...listProps} />;
 };
