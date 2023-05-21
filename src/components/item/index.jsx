84782a0128da718d5a63f302f0d4ed82ca3a7acd
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-05-21 15:09:49 -0600

    it adds dahsboard to item component

--- src/components/item/index.jsx
+++ src/components/item/index.jsx
@@ -10,6 +10,7 @@ import { Button } from 'primereact/button';
 import { Menubar } from 'primereact/menubar';
 import { Dropdown } from 'primereact/dropdown';
 import { InputText } from 'primereact/inputtext';
+import { Dashboard } from '@components/dashboard';
 import { InputTextarea } from 'primereact/inputtextarea';
 
 //Custom Components
@@ -49,64 +50,66 @@ export const Item = (props) => {
     };
 
     return (
-        <Panel headerTemplate={toolbar}>
-            <div className="">
-                <div className="p-fluid formgrid grid">
-                    <div className="field col-4">
-                        <label>Nombre del Articulo</label>
-                        <div className="p-inputgroup">
-                            <InputText
-                                value={item[fields.ITEM_NAME]}
+        <Dashboard>
+            <Panel headerTemplate={toolbar}>
+                <div className="">
+                    <div className="p-fluid formgrid grid">
+                        <div className="field col-4">
+                            <label>Nombre del Articulo</label>
+                            <div className="p-inputgroup">
+                                <InputText
+                                    value={item[fields.ITEM_NAME]}
+                                    onChange={(event) => {
+                                        updateField(fields.ITEM_NAME, event);
+                                    }}
+                                />
+                                <Button icon="pi pi-search" onClick={showSearch} />
+                            </div>
+                        </div>
+                        <div className="field col-4">
+                            <label>Metodo de valuacion</label>
+                            <Dropdown
+                                value={item[fields.VALUATION_TYPE]}
+                                options={options.valuation}
                                 onChange={(event) => {
-                                    updateField(fields.ITEM_NAME, event);
+                                    updateField(fields.VALUATION_TYPE, event);
                                 }}
                             />
-                            <Button icon="pi pi-search" onClick={showSearch} />
                         </div>
-                    </div>
-                    <div className="field col-4">
-                        <label>Metodo de valuacion</label>
-                        <Dropdown
-                            value={item[fields.VALUATION_TYPE]}
-                            options={options.valuation}
-                            onChange={(event) => {
-                                updateField(fields.VALUATION_TYPE, event);
-                            }}
-                        />
-                    </div>
-                    <div className="field col-4">
-                        <label>Estado</label>
-                        <div className="flex justify-content-center flex-wrap">
-                            <Chip
-                                label={usedLabel}
-                                icon={usedIcon}
-                                className="mr-2 mb-2 custom-chip"
+                        <div className="field col-4">
+                            <label>Estado</label>
+                            <div className="flex justify-content-center flex-wrap">
+                                <Chip
+                                    label={usedLabel}
+                                    icon={usedIcon}
+                                    className="mr-2 mb-2 custom-chip"
+                                />
+                            </div>
+                        </div>
+                        <div className="field col-12">
+                            <label>Descripcion</label>
+                            <InputTextarea
+                                value={item[fields.DESCRIPTION]}
+                                onChange={(event) => {
+                                    updateField(fields.DESCRIPTION, event);
+                                }}
+                                rows={10}
+                                cols={30}
+                                autoResize
                             />
                         </div>
                     </div>
-                    <div className="field col-12">
-                        <label>Descripcion</label>
-                        <InputTextarea
-                            value={item[fields.DESCRIPTION]}
-                            onChange={(event) => {
-                                updateField(fields.DESCRIPTION, event);
-                            }}
-                            rows={10}
-                            cols={30}
-                            autoResize
-                        />
-                    </div>
                 </div>
-            </div>
-            <Toast ref={notification} />
-            <Search
-                visible={searchVisible}
-                onHide={hideSearch}
-                fields={options.searchFields}
-                selectOption={selectOption}
-                getDataAsPage={getDataAsPage}
-                getDataByPage={getDataByPage}
-            />
-        </Panel>
+                <Toast ref={notification} />
+                <Search
+                    visible={searchVisible}
+                    onHide={hideSearch}
+                    fields={options.searchFields}
+                    selectOption={selectOption}
+                    getDataAsPage={getDataAsPage}
+                    getDataByPage={getDataByPage}
+                />
+            </Panel>
+        </Dashboard>
     );
 };
