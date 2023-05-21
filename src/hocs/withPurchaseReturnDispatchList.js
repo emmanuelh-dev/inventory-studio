ca49906770bc70d1b2a8e1868ffbc71d94f0e3d0
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-05-21 13:38:29 -0600

    it implements services

--- src/hocs/withPurchaseReturnDispatchList.js
+++ src/hocs/withPurchaseReturnDispatchList.js
@@ -1,17 +1,38 @@
+import { DOCUMENT_TYPES } from '@constants';
+import services from '@services/api-services';
 import { documentSearchFields, documentFilters } from '@constants/options';
 
 export const withPurchaseReturnDispatchList = (WrappedComponent) => (props) => {
-    const endpoint = {
-        redirect: 'dispatch/PURCHASE_RETURN/id',
-        search: process.env.NEXT_PUBLIC_PR_DISPATCHES_SEARCH,
-        suggestions: process.env.NEXT_PUBLIC_PR_DISPATCHES_SUGGESTIONS,
+    const getDataAsPage = async () => {
+        const result = await services.findAllDispatchDocumentAsPage(DOCUMENT_TYPES.PURCHASE_RETURN);
+        return result;
     };
 
-    return (
-        <WrappedComponent
-            endpoint={endpoint}
-            filters={documentFilters}
-            fields={documentSearchFields}
-        />
-    );
+    const getDataByPage = async (page) => {
+        const result = await services.findAllDispatchDocumentByPage(
+            DOCUMENT_TYPES.PURCHASE_RETURN,
+            page
+        );
+        return result;
+    };
+
+    const getDataByFilter = async (filter) => {
+        const result = await services.findDispatchDocumentByFilter(
+            DOCUMENT_TYPES.PURCHASE_RETURN,
+            filter
+        );
+        return result;
+    };
+
+    const listProps = {
+        filters: documentFilters,
+        type: DOCUMENT_TYPES.INPUT,
+        fields: documentSearchFields,
+        getDataAsPage: getDataAsPage,
+        getDataByPage: getDataByPage,
+        redirect: 'dispatch/PURCHASE_RETURN/id/',
+        getDataByFilter: getDataByFilter,
+    };
+
+    return <WrappedComponent {...listProps} />;
 };
