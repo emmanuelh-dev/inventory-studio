0702485d0e6b3a8f658413168f8f74444ea9e4ca
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-05-22 22:29:42 -0600

    it adds warehouse to dashboard

--- src/components/warehouse/index.jsx
+++ src/components/warehouse/index.jsx
@@ -10,6 +10,7 @@ import { InputText } from 'primereact/inputtext';
 
 //Custom Components
 import { Search } from '@components/search';
+import { Dashboard } from '@components/dashboard';
 
 export const Warehouse = (props) => {
     const {
@@ -34,41 +35,43 @@ export const Warehouse = (props) => {
     };
 
     return (
-        <Panel headerTemplate={toolbar}>
-            <div className="">
-                <div className="p-fluid formgrid grid">
-                    <div className="field col-6">
-                        <label>Nombre del Almacen</label>
-                        <div className="p-inputgroup">
-                            <InputText
-                                value={warehouse[fields.WAREHOUSE_NAME]}
-                                onChange={(event) => {
-                                    updateField(fields.WAREHOUSE_NAME, event);
-                                }}
-                            />
-                            <Button icon="pi pi-search" onClick={showSearch} />
+        <Dashboard>
+            <Panel headerTemplate={toolbar}>
+                <div className="">
+                    <div className="p-fluid formgrid grid">
+                        <div className="field col-6">
+                            <label>Nombre del Almacen</label>
+                            <div className="p-inputgroup">
+                                <InputText
+                                    value={warehouse[fields.WAREHOUSE_NAME]}
+                                    onChange={(event) => {
+                                        updateField(fields.WAREHOUSE_NAME, event);
+                                    }}
+                                />
+                                <Button icon="pi pi-search" onClick={showSearch} />
+                            </div>
                         </div>
-                    </div>
-                    <div className="field col-6">
-                        <label>Estado</label>
-                        <div className="flex justify-content-center flex-wrap">
-                            <Chip
-                                label={usedLabel}
-                                icon={usedIcon}
-                                className="mr-2 mb-2 custom-chip"
-                            />
+                        <div className="field col-6">
+                            <label>Estado</label>
+                            <div className="flex justify-content-center flex-wrap">
+                                <Chip
+                                    label={usedLabel}
+                                    icon={usedIcon}
+                                    className="mr-2 mb-2 custom-chip"
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <Toast ref={notification} />
-            <Search
-                visible={searchVisible}
-                onHide={hideSearch}
-                fields={options.searchFields}
-                selectOption={selectOption}
-                endpoint={endpoint}
-            />
-        </Panel>
+                <Toast ref={notification} />
+                <Search
+                    visible={searchVisible}
+                    onHide={hideSearch}
+                    fields={options.searchFields}
+                    selectOption={selectOption}
+                    endpoint={endpoint}
+                />
+            </Panel>
+        </Dashboard>
     );
 };
