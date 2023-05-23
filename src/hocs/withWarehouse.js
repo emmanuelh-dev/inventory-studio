e011c09479bf721f1fee000eeee329b02b9f4d25
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-05-22 23:20:21 -0600

    it removes commented code

--- src/hocs/withWarehouse.js
+++ src/hocs/withWarehouse.js
@@ -15,12 +15,6 @@ import { useForm, useStateStatus } from '@hooks/useFormState';
 export const withWarehouse = (WrappedComponent) => (props) => {
     const fields = { ...warehouseFields };
     const { initialState } = { ...props };
-    // const endpoint = {
-    //     search: process.env.NEXT_PUBLIC_ITEMS_SEARCH,
-    //     suggestions: process.env.NEXT_PUBLIC_WAREHOUSES_SUGGESTIONS,
-    //     save: process.env.NEXT_PUBLIC_WAREHOUSES_SAVE,
-    //     update: process.env.NEXT_PUBLIC_WAREHOUSES_SAVE,
-    // };
     const {
         form,
         clearForm,
@@ -30,16 +24,7 @@ export const withWarehouse = (WrappedComponent) => (props) => {
         updateFormFromService,
         updateSaveButtonStatus,
     } = useForm(initialState, warehouseState);
-
-    // initialState =
-    //     initialState == undefined || initialState == null ? warehouseState : initialState;
-
-    // const { state, updateField, updateState } = useFormState(initialState);
-    // const { buttonState, updateCopy, updateSaveButton } = useCopy(initialState, state);
-
-    // const { onNew } = useNew(updateState, updateCopy, warehouseState);
     const { notification, showNotification } = useNotification();
-    // const { onSave } = useSave(warehouseFields.ID, endpoint, showNotification);
     const { usedIcon, usedLabel, updateStateStatus } = useStateStatus(form, fields.USED);
     const { search, showSearch, hideSearch, selectOption } = useSearch(updateForm, updateFormCopy);
 
@@ -109,14 +94,6 @@ export const withWarehouse = (WrappedComponent) => (props) => {
         return validateNameField;
     };
 
-    // const _toolbar = [...toolbar];
-    // _toolbar[0].command = onNew;
-    // _toolbar[1].command = () => {
-    //     onSave(state, updateState, updateCopy);
-    // };
-    // _toolbar[1].disabled = buttonState;
-    // _toolbar[2].disabled = state[warehouseFields.USED] || !state[warehouseFields.ID];
-
     const options = {
         toolbar: createWarehouseToolbar(onNew(), onSave(), onCancel(), onDelete()),
         searchFields: warehouseSearchFields,
@@ -142,22 +119,6 @@ export const withWarehouse = (WrappedComponent) => (props) => {
     };
 
     return <WrappedComponent {...componentProps} />;
-    // return (
-    //     <WrappedComponent
-    //         warehouse={state}
-    //         updateField={updateField}
-    //         fields={warehouseFields}
-    //         options={options}
-    //         usedIcon={usedIcon}
-    //         usedLabel={usedLabel}
-    //         notification={notification}
-    //         searchVisible={search}
-    //         showSearch={showSearch}
-    //         hideSearch={hideSearch}
-    //         selectOption={selectOption}
-    //         endpoint={endpoint}
-    //     />
-    // );
 };
 
 const createWarehouseToolbar = (onNew, onSave, onCancel, onDelete) => {
