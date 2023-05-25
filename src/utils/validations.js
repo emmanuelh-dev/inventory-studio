7a77119ac643d57b57a50c72d615dcd41c8bc051
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-05-25 15:13:28 -0600

    Refactor: It improves validate repeated item

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -3,21 +3,23 @@ import { MESSAGE_TYPES, validationFields } from '@constants';
 
 const fields = validationFields;
 
-export const validateRepeatedItem = (detail, details, showNotification) => {
-    const validate = details.find(
-        (element) =>
-            element[fields.ITEM][fields.ID] == detail[fields.ITEM][fields.ID] &&
-            element[fields.LINE_NUMBER] !== detail[fields.LINE_NUMBER]
-    );
+const ifItemPresent = (element, detail) => {
+    const isSameItem = element[fields.ITEM][fields.ID] == detail[fields.ITEM][fields.ID];
+    const isDeletedRow = element[fields.DELETED];
 
-    validate = validate != undefined;
-    if (validate) {
-        const message = `El articulo ${
-            detail[fields.ITEM].itemName
-        } ya se encuentra en este documento`;
+    return isSameItem && !isDeletedRow;
+};
+
+export const validateRepeatedItem = (detail, details, showNotification) => {
+    const isRepeated = details.find((element) => ifItemPresent(element, detail));
+
+    isRepeated = isRepeated != undefined;
+    if (isRepeated) {
+        const itemName = detail[fields.ITEM][fields.ITEM_NAME];
+        const message = `El articulo ${itemName} ya se encuentra en este documento`;
         showNotification(MESSAGE_TYPES.ERROR, message);
     }
-    return validate;
+    return isRepeated;
 };
 
 export const validateNotEmptyField = (value, fieldLabel, showNotification) => {
