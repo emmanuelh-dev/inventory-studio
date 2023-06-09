f40491501e6d97887d60b89d09e5c0d3d754693c
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-06-08 23:47:24 -0600

    it moves the functionality to tge hoc

--- src/components/inputbarcodereader/index.jsx
+++ src/components/inputbarcodereader/index.jsx
@@ -1,48 +1,15 @@
 import { useState } from 'react';
-import { getValue } from '@utils';
 import services from '@services/api-services';
-import { MESSAGE_TYPES, detailState } from '@constants';
 import { InputText } from 'primereact/inputtext';
+import { getValue, ifItemPresent, isObjectEmpty } from '@utils';
 
 export const InputBarcodeReader = (props) => {
     const [barcode, setBarcode] = useState('');
-    const { processBarcode, disabled, warehouse, showNotification } = { ...props };
+    const { processBarcode, disabled } = { ...props };
 
     const readBarcode = async (value) => {
-        const detail = { ...detailState };
-        try {
-            const response = await services.findDispatchDetailReadingBarcode(warehouse, value);
-            detail.quantity = 1;
-            detail.item = response.item;
-            detail.unitPrice = response.unitPrice;
-            detail.totalPrice = response.unitPrice;
-            detail.description = response.description;
-            processBarcode(detail);
-            setBarcode('');
-        } catch (error) {
-            showNotification(MESSAGE_TYPES.ERROR, error.message);
-        }
-
-        // const params = {
-        //     warehouse,
-        //     type: documentType,
-        //     barcode: value,
-        //     format: 'CODE128',
-        // };
-        // const url = replaceParams(endpoint.readBarcode, params);
-        // useGet(url)
-        //     .then((data) => {
-        // detail.item = data.item;
-        // detail.quantity = 1;
-        // detail.unitPrice = data.unitPrice;
-        // detail.totalPrice = data.unitPrice;
-        // detail.description = data.description;
-        // processBarcode(detail);
-        // setBarcode('');
-        //     })
-        //     .catch((error) => {
-        //         showNotification('error', error.message);
-        //     });
+        processBarcode(value);
+        setBarcode('');
     };
 
     const onChange = (event) => {
