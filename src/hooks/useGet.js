14991f0f5061a54c4d10c45c0873b705eec9b837
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-06-11 14:54:47 -0600

    if the error is 401 then signout

--- src/hooks/useGet.js
+++ src/hooks/useGet.js
@@ -1,15 +1,22 @@
-import { getSession } from 'next-auth/react';
+import { signOut, getSession } from 'next-auth/react';
 import { buildFetchOptions } from '@utils/request';
 export const useGet = async (url) => {
     const session = await getSession();
     const options = buildFetchOptions('GET', session, {});
     const response = await fetch(url, options);
-
+    const body = await response.json();
     if (!response.ok) {
-        const body = await response.json();
+        await UnauthorizedHandleResponse(response, body);
+    }
+    return body;
+};
+
+const UnauthorizedHandleResponse = async (response, body) => {
+    if (response.status != 401) {
         throw new Error(body.message);
     }
 
-    const data = await response.json();
-    return data;
+    if (body.error == 'invalid_token') {
+        await signOut({ callbackUrl: '/login' });
+    }
 };
