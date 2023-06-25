6cffa8e93145b380b1fdc1e8c1db72e4f8a302fb
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-06-25 23:55:02 +0000

    it fixes error on withAuthentication hoc

--- src/hocs/withAuthentication.js
+++ src/hocs/withAuthentication.js
@@ -1,53 +1,57 @@
 import { useRouter } from 'next/router';
 import { signIn } from 'next-auth/react';
 import React, { useState, useRef } from 'react';
-export const withAuthentication = (WrappedComponent) => (props) => {
-    const router = useRouter();
-    const message = useRef(null);
+export const withAuthentication = (WrappedComponent) => {
+    const WithAuthentication = (props) => {
+        const router = useRouter();
+        const message = useRef(null);
 
-    const [userAccount, setUserAccount] = useState({
-        username: '',
-        password: '',
-    });
-
-    const handleInputChange = (event) => {
-        setUserAccount({
-            ...userAccount,
-            [event.target.name]: event.target.value,
+        const [userAccount, setUserAccount] = useState({
+            username: '',
+            password: '',
         });
+
+        const handleInputChange = (event) => {
+            setUserAccount({
+                ...userAccount,
+                [event.target.name]: event.target.value,
+            });
+        };
+
+        const handleFormSubmit = async (event) => {
+            event.preventDefault();
+            const result = await signIn('credentials', {
+                username: userAccount.username,
+                password: userAccount.password,
+                redirect: false,
+            });
+            if (result.ok) {
+                router.push('dashboard');
+            } else {
+                showMessage();
+            }
+        };
+
+        const showMessage = () => {
+            message.current.show([
+                {
+                    severity: 'error',
+                    summary: '',
+                    detail: 'Usuario o contraseña son incorrectos',
+                    sticky: true,
+                },
+            ]);
+        };
+
+        const authenticationProps = {
+            message,
+            userAccount,
+            handleFormSubmit,
+            handleInputChange,
+        };
+
+        return <WrappedComponent {...authenticationProps} />;
     };
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        const result = await signIn('credentials', {
-            username: userAccount.username,
-            password: userAccount.password,
-            redirect: false,
-        });
-        if (result.ok) {
-            router.push('dashboard');
-        } else {
-            showMessage();
-        }
-    };
-
-    const showMessage = () => {
-        message.current.show([
-            {
-                severity: 'error',
-                summary: '',
-                detail: 'Usuario o contraseña son incorrectos',
-                sticky: true,
-            },
-        ]);
-    };
-
-    const authenticationProps = {
-        message,
-        userAccount,
-        handleFormSubmit,
-        handleInputChange,
-    };
-
-    return <WrappedComponent {...authenticationProps} />;
+    return WithAuthentication;
 };
