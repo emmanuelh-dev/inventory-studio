01c6162fe49743a2724680a6e489c5800e6844d6
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-06-25 08:06:17 +0000

    it fixes errors marked by eslint

--- src/hocs/withWarehouse.js
+++ src/hocs/withWarehouse.js
@@ -11,112 +11,122 @@ import { useSearch } from '@hooks/useSearch';
 import { useNotification } from '@hooks/useNotification';
 import { useForm, useStateStatus } from '@hooks/useFormState';
 
-export const withWarehouse = (WrappedComponent) => (props) => {
-    const fields = { ...warehouseFields };
-    const { initialState } = { ...props };
-    const {
-        form,
-        clearForm,
-        updateForm,
-        updateFormCopy,
-        updateFormField,
-        updateFormFromService,
-        updateSaveButtonStatus,
-    } = useForm(initialState, warehouseState);
-    const { notification, showNotification } = useNotification();
-    const { usedIcon, usedLabel, updateStateStatus } = useStateStatus(form, fields.USED);
-    const { search, showSearch, hideSearch, selectOption } = useSearch(updateForm, updateFormCopy);
-
-    const onNew = () => {
-        return {
-            command: clearForm,
-        };
-    };
-
-    const onSave = () => {
-        const onSaveWarehouse = async () => {
-            const validation = saveValidations();
-            if (validation) {
-                try {
-                    const isNew = isNullOrUndefinedOrEmptyString(form[fields.ID]);
-                    const response = isNew
-                        ? await services.postWarehouse(form)
-                        : await services.putWarehouse(form);
-                    updateFormFromService(response);
-                    isNew
-                        ? showNotification(MESSAGE_TYPES.SUCCESS)
-                        : showNotification(MESSAGE_TYPES.SUCCESS, MESSAGES.SUCCESS_RECORD_UPDATED);
-                } catch (error) {
-                    showNotification(MESSAGE_TYPES.ERROR, error.message);
-                }
-            }
-        };
-
-        return {
-            command: onSaveWarehouse,
-            disabled: updateSaveButtonStatus(),
-        };
-    };
-
-    const onCancel = () => {
-        const onCancelWarehouse = async () => {
-            if (!isNullOrUndefinedOrEmptyString(form[fields.ID])) {
-                const response = await services.findWarehouseById(form[fields.ID]);
-                updateFormFromService(response);
-            } else {
-                clearForm();
-            }
-        };
-
-        return {
-            command: onCancelWarehouse,
-            disabled: updateSaveButtonStatus(),
-        };
-    };
-
-    const onDelete = () => {
-        const onDeleteWarehouse = async () => {};
-
-        return {
-            command: onDeleteWarehouse,
-            disabled: form[fields.USED] || isNullOrUndefinedOrEmptyString(form[fields.ID]),
-        };
-    };
-
-    const saveValidations = () => {
-        const validateNameField = validateNotEmptyField(
-            form[fields.WAREHOUSE_NAME],
-            'Nombre',
-            showNotification
+export const withWarehouse = (WrappedComponent) => {
+    const WithWarehouse = (props) => {
+        const fields = { ...warehouseFields };
+        const { initialState } = { ...props };
+        const {
+            form,
+            clearForm,
+            updateForm,
+            updateFormCopy,
+            updateFormField,
+            updateFormFromService,
+            updateSaveButtonStatus,
+        } = useForm(initialState, warehouseState);
+        const { notification, showNotification } = useNotification();
+        const { usedIcon, usedLabel, updateStateStatus } = useStateStatus(form, fields.USED);
+        const { search, showSearch, hideSearch, selectOption } = useSearch(
+            updateForm,
+            updateFormCopy
         );
 
-        return validateNameField;
+        const onNew = () => {
+            return {
+                command: clearForm,
+            };
+        };
+
+        const onSave = () => {
+            const onSaveWarehouse = async () => {
+                const validation = saveValidations();
+                if (validation) {
+                    try {
+                        const isNew = isNullOrUndefinedOrEmptyString(form[fields.ID]);
+                        const response = isNew
+                            ? await services.postWarehouse(form)
+                            : await services.putWarehouse(form);
+                        updateFormFromService(response);
+                        isNew
+                            ? showNotification(MESSAGE_TYPES.SUCCESS)
+                            : showNotification(
+                                  MESSAGE_TYPES.SUCCESS,
+                                  MESSAGES.SUCCESS_RECORD_UPDATED
+                              );
+                    } catch (error) {
+                        showNotification(MESSAGE_TYPES.ERROR, error.message);
+                    }
+                }
+            };
+
+            return {
+                command: onSaveWarehouse,
+                disabled: updateSaveButtonStatus(),
+            };
+        };
+
+        const onCancel = () => {
+            const onCancelWarehouse = async () => {
+                if (!isNullOrUndefinedOrEmptyString(form[fields.ID])) {
+                    const response = await services.findWarehouseById(form[fields.ID]);
+                    updateFormFromService(response);
+                } else {
+                    clearForm();
+                }
+            };
+
+            return {
+                command: onCancelWarehouse,
+                disabled: updateSaveButtonStatus(),
+            };
+        };
+
+        const onDelete = () => {
+            const onDeleteWarehouse = async () => {};
+
+            return {
+                command: onDeleteWarehouse,
+                disabled: form[fields.USED] || isNullOrUndefinedOrEmptyString(form[fields.ID]),
+            };
+        };
+
+        const saveValidations = () => {
+            const validateNameField = validateNotEmptyField(
+                form[fields.WAREHOUSE_NAME],
+                'Nombre',
+                showNotification
+            );
+
+            return validateNameField;
+        };
+
+        const options = {
+            toolbar: createWarehouseToolbar(onNew(), onSave(), onCancel(), onDelete()),
+            searchFields: warehouseSearchFields,
+        };
+
+        useEffect(() => {
+            updateStateStatus();
+        }, [form]);
+
+        const componentProps = {
+            fields: fields,
+            warehouse: form,
+            options: options,
+            usedIcon: usedIcon,
+            usedLabel: usedLabel,
+            searchVisible: search,
+            showSearch: showSearch,
+            hideSearch: hideSearch,
+            selectOption: selectOption,
+            notification: notification,
+            updateField: updateFormField,
+        };
+
+        return <WrappedComponent {...componentProps} />;
     };
 
-    const options = {
-        toolbar: createWarehouseToolbar(onNew(), onSave(), onCancel(), onDelete()),
-        searchFields: warehouseSearchFields,
-    };
-
-    useEffect(() => {
-        updateStateStatus();
-    }, [form]);
-
-    const componentProps = {
-        fields: fields,
-        warehouse: form,
-        options: options,
-        usedIcon: usedIcon,
-        usedLabel: usedLabel,
-        searchVisible: search,
-        showSearch: showSearch,
-        hideSearch: hideSearch,
-        selectOption: selectOption,
-        notification: notification,
-        updateField: updateFormField,
-    };
-
-    return <WrappedComponent {...componentProps} />;
+    return WithWarehouse;
 };
 
 const createWarehouseToolbar = (onNew, onSave, onCancel, onDelete) => {
--- src/hocs/withWarehouseList.js
+++ src/hocs/withWarehouseList.js
@@ -1,30 +1,34 @@
 import services from '@services/api-services';
 import { warehouseSearchFields, warehouseFilters } from '@constants/options';
 
-export const withWarehouseList = (WrappedComponent) => (props) => {
-    const getDataAsPage = async () => {
-        const result = await services.findAllWarehousesAsPage();
-        return result;
+export const withWarehouseList = (WrappedComponent) => {
+    const WithWarehouseList = () => {
+        const getDataAsPage = async () => {
+            const result = await services.findAllWarehousesAsPage();
+            return result;
+        };
+
+        const getDataByPage = async (page) => {
+            const result = await services.findAllWarehousesByPage(page);
+            return result;
+        };
+
+        const getDataByFilter = async (filter) => {
+            const result = await services.findWarehousesByFilter(filter);
+            return result;
+        };
+
+        const listProps = {
+            redirect: 'warehouse/',
+            filters: warehouseFilters,
+            fields: warehouseSearchFields,
+            getDataAsPage: getDataAsPage,
+            getDataByPage: getDataByPage,
+            getDataByFilter: getDataByFilter,
+        };
+
+        return <WrappedComponent {...listProps} />;
     };
 
-    const getDataByPage = async (page) => {
-        const result = await services.findAllWarehousesByPage(page);
-        return result;
-    };
-
-    const getDataByFilter = async (filter) => {
-        const result = await services.findWarehousesByFilter(filter);
-        return result;
-    };
-
-    const listProps = {
-        redirect: 'warehouse/',
-        filters: warehouseFilters,
-        fields: warehouseSearchFields,
-        getDataAsPage: getDataAsPage,
-        getDataByPage: getDataByPage,
-        getDataByFilter: getDataByFilter,
-    };
-
-    return <WrappedComponent {...listProps} />;
+    return WithWarehouseList;
 };
