31f4444817a5e57f3096a76a874f47e40410b58a
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-06-25 08:09:26 +0000

    it fixes errors marked by eslint

--- src/hocs/withItemList.js
+++ src/hocs/withItemList.js
@@ -1,30 +1,34 @@
 import services from '@services/api-services';
 import { itemSearchFields, itemFilters } from '@constants/options';
 
-export const withItemList = (WrappedComponent) => (props) => {
-    const getDataAsPage = async () => {
-        const result = await services.findAllItemsAsPage();
-        return result;
+export const withItemList = (WrappedComponent) => {
+    const WithItemList = (props) => {
+        const getDataAsPage = async () => {
+            const result = await services.findAllItemsAsPage();
+            return result;
+        };
+
+        const getDataByPage = async (page) => {
+            const result = await services.findAllItemsByPage(page);
+            return result;
+        };
+
+        const getDataByFilter = async (filter) => {
+            const result = await services.findItemsByFilter(filter);
+            return result;
+        };
+
+        const listProps = {
+            redirect: 'item/',
+            filters: itemFilters,
+            fields: itemSearchFields,
+            getDataAsPage: getDataAsPage,
+            getDataByPage: getDataByPage,
+            getDataByFilter: getDataByFilter,
+        };
+
+        return <WrappedComponent {...listProps} />;
     };
 
-    const getDataByPage = async (page) => {
-        const result = await services.findAllItemsByPage(page);
-        return result;
-    };
-
-    const getDataByFilter = async (filter) => {
-        const result = await services.findItemsByFilter(filter);
-        return result;
-    };
-
-    const listProps = {
-        redirect: 'item/',
-        filters: itemFilters,
-        fields: itemSearchFields,
-        getDataAsPage: getDataAsPage,
-        getDataByPage: getDataByPage,
-        getDataByFilter: getDataByFilter,
-    };
-
-    return <WrappedComponent {...listProps} />;
+    return WithItemList;
 };
