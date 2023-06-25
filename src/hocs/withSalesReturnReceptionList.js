b4b6c7ffa3a6bfcf9f52e58d5ae84dc0effb3ac7
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-06-25 08:12:16 +0000

    it fixes errors marked by eslint

--- src/hocs/withSalesReturnReceptionList.js
+++ src/hocs/withSalesReturnReceptionList.js
@@ -2,36 +2,42 @@ import { DOCUMENT_TYPES } from '@constants';
 import services from '@services/api-services';
 import { documentSearchFields, documentFilters } from '@constants/options';
 
-export const withSalesReturnReceptionList = (WrappedComponent) => (props) => {
-    const getDataAsPage = async () => {
-        const result = await services.findAllReceptionDocumentAsPage(DOCUMENT_TYPES.SALES_RETURN);
-        return result;
+export const withSalesReturnReceptionList = (WrappedComponent) => {
+    const WithSalesReturnReceptionList = () => {
+        const getDataAsPage = async () => {
+            const result = await services.findAllReceptionDocumentAsPage(
+                DOCUMENT_TYPES.SALES_RETURN
+            );
+            return result;
+        };
+
+        const getDataByPage = async (page) => {
+            const result = await services.findAllReceptionDocumentByPage(
+                DOCUMENT_TYPES.SALES_RETURN,
+                page
+            );
+            return result;
+        };
+
+        const getDataByFilter = async (filter) => {
+            const result = await services.findReceptionDocumentByFilter(
+                DOCUMENT_TYPES.SALES_RETURN,
+                filter
+            );
+            return result;
+        };
+
+        const listProps = {
+            filters: documentFilters,
+            fields: documentSearchFields,
+            getDataAsPage: getDataAsPage,
+            getDataByPage: getDataByPage,
+            redirect: 'reception/SALES_RETURN/id/',
+            getDataByFilter: getDataByFilter,
+        };
+
+        return <WrappedComponent {...listProps} />;
     };
 
-    const getDataByPage = async (page) => {
-        const result = await services.findAllReceptionDocumentByPage(
-            DOCUMENT_TYPES.SALES_RETURN,
-            page
-        );
-        return result;
-    };
-
-    const getDataByFilter = async (filter) => {
-        const result = await services.findReceptionDocumentByFilter(
-            DOCUMENT_TYPES.SALES_RETURN,
-            filter
-        );
-        return result;
-    };
-
-    const listProps = {
-        filters: documentFilters,
-        fields: documentSearchFields,
-        getDataAsPage: getDataAsPage,
-        getDataByPage: getDataByPage,
-        redirect: 'reception/SALES_RETURN/id/',
-        getDataByFilter: getDataByFilter,
-    };
-
-    return <WrappedComponent {...listProps} />;
+    return WithSalesReturnReceptionList;
 };
