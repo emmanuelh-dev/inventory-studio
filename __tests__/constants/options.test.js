9fa9cd60af0ea3823a48139dfd4d37fb9d181fa2
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-07-09 18:38:36 +0000

    it adds test fot reception types

--- __tests__/constants/options.test.js
+++ __tests__/constants/options.test.js
@@ -1,5 +1,4 @@
-import { valuation } from '@constants/options';
-import { toHaveValue } from '@testing-library/jest-dom/matchers';
+import { valuation, receptionTypes } from '@constants/options';
 describe('Options', () => {
     describe('valuation', () => {
         it('has only average valuation type as object', () => {
@@ -7,6 +6,24 @@ describe('Options', () => {
             expect(valuation.length).toBe(1);
             expect(result.value).toBe('AVERAGE');
             expect(result.label).toBe('Promedio Ponderado');
+        });
+    });
+
+    describe('receptionTypes', () => {
+        it('has two element in the array', () => {
+            expect(receptionTypes.length).toBe(2);
+        });
+
+        it('object of type input on the first place', () => {
+            const result = receptionTypes[0];
+            expect(result.label).toBe('Ingreso');
+            expect(result.value).toBe('INPUT');
+        });
+
+        it('object of type sales return on the second place', () => {
+            const result = receptionTypes[1];
+            expect(result.label).toBe('Devolucion por venta');
+            expect(result.value).toBe('SALES_RETURN');
         });
     });
 });
