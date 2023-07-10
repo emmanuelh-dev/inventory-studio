7e3430a6da2ce38666b44988c262817a9a220112
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-07-10 17:35:01 +0000

    it adds test for toolbar

--- __tests__/constants/options.test.js
+++ __tests__/constants/options.test.js
@@ -1,4 +1,10 @@
-import { valuation, receptionTypes, dispatchTypes, documentTypes } from '@constants/options';
+import {
+    valuation,
+    receptionTypes,
+    dispatchTypes,
+    documentTypes,
+    toolbar,
+} from '@constants/options';
 describe('Options', () => {
     describe('valuation', () => {
         it('has only average valuation type as object', () => {
@@ -51,6 +57,42 @@ describe('Options', () => {
             expect(documentTypes.OUTPUT).toBe('Salida');
             expect(documentTypes.SALE_RETURN).toBe('Devolucion por venta');
             expect(documentTypes.PURCHASE_RETURN).toBe('Devolucion por compra');
+        });
+    });
+
+    describe('toolbar', () => {
+        it('has array with five elements for basic toolbar', () => {
+            expect(toolbar.length).toBe(5);
+        });
+
+        it('should have correct label and icon value for Nuevo element', () => {
+            const result = toolbar[0];
+            expect(result.label).toBe('Nuevo');
+            expect(result.icon).toBe('pi pi-fw pi-plus');
+        });
+
+        it('should have correct label and icon value for Guardar element', () => {
+            const result = toolbar[1];
+            expect(result.label).toBe('Guardar');
+            expect(result.icon).toBe('pi pi-fw pi-save');
+        });
+
+        it('should have correct label and icon value for Cancelar element', () => {
+            const result = toolbar[2];
+            expect(result.label).toBe('Cancelar');
+            expect(result.icon).toBe('pi pi-fw pi-times');
+        });
+
+        it('should have correct label and icon value for Eliminar element', () => {
+            const result = toolbar[3];
+            expect(result.label).toBe('Eliminar');
+            expect(result.icon).toBe('pi pi-fw pi-trash');
+        });
+
+        it('should have correct label and items value for Acciones element', () => {
+            const result = toolbar[4];
+            expect(result.label).toBe('Acciones');
+            expect(result.items).toEqual([]);
         });
     });
 });
