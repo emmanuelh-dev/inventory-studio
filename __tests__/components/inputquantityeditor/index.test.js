e82df83c5b80258af0dd54b071e5558c5483ea17
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-08-27 18:55:47 +0000

    it add validation to check format

--- __tests__/components/inputquantityeditor/index.test.js
+++ __tests__/components/inputquantityeditor/index.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { InputQuantityEditor } from '@components/inputquantityeditor';
 describe('InputQuantityEditor', () => {
     const detail = {
@@ -21,4 +21,23 @@ describe('InputQuantityEditor', () => {
         expect(element).toBeVisible();
         expect(element.value).toBe('0');
     });
+
+    it('should change value', () => {
+        render(<InputQuantityEditor {...inputquantityeditorProps} />);
+        const element = screen.getByRole('spinbutton');
+        fireEvent.change(element, { target: { value: 5 } });
+        expect(element).toBeVisible();
+        expect(element.value).toBe('5');
+    });
+
+    it('should have format ###,####,###', () => {
+        render(<InputQuantityEditor {...inputquantityeditorProps} />);
+        const element = screen.getByRole('spinbutton');
+        fireEvent.change(element, { target: { value: 1000000 } });
+        fireEvent.blur(element);
+        expect(element).toBeVisible();
+        expect(element).toHaveValue('1,000,000');
+    });
+
+    it('should accept only integers values', () => {});
 });
