97154425d8846d6f7e97656b56371d97e52d3e2a
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-08-27 08:02:39 +0000

    it adds basic test for input quantity editor

--- __tests__/components/inputquantityeditor/index.test.js
+++ __tests__/components/inputquantityeditor/index.test.js
@@ -1,4 +1,23 @@
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import { InputQuantityEditor } from '@components/inputquantityeditor';
 describe('InputQuantityEditor', () => {
-    it('should be render', () => {});
+    const detail = {
+        quantity: 0,
+    };
+
+    const updateFieldMock = jest.fn((field, event) => {
+        detail[field] = event.value;
+    });
+
+    it('should be render', () => {
+        const inputquantityeditorProps = {
+            row: detail,
+            field: 'quantity',
+            updateField: updateFieldMock,
+        };
+        render(<InputQuantityEditor {...inputquantityeditorProps} />);
+        const element = screen.getByRole('spinbutton');
+        expect(element).toBeVisible();
+    });
 });
