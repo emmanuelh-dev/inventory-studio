d9706b633fc071bf04c71b9e3e20a3e6eae1e32d
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-09-04 00:49:50 +0000

    it adds test for item dropdown that checks  values

--- src/components/itemdropdown/index.test.js
+++ src/components/itemdropdown/index.test.js
@@ -22,4 +22,21 @@ describe('ItemDropdown', () => {
         const element = await screen.findByTestId('item-dropdown');
         expect(element).toBeVisible();
     });
+
+    it('should have three elements', async () => {
+        render(<ItemDropdown {...itemDropDownProps} test="second" />);
+        const dropdown = await screen.findByTestId('item-dropdown');
+        fireEvent.click(dropdown);
+        const options = await screen.findAllByText(/item/i);
+        expect(options).toHaveLength(3);
+        const itemOne = options[0];
+        const itemTwo = options[1];
+        const itemThree = options[2];
+        expect(itemOne).toBeInTheDocument();
+        expect(itemOne).toHaveTextContent('item one');
+        expect(itemTwo).toBeInTheDocument();
+        expect(itemTwo).toHaveTextContent('item two');
+        expect(itemThree).toBeInTheDocument();
+        expect(itemThree).toHaveTextContent('item three');
+    });
 });
