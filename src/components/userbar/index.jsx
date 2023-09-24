dddff6d1fc800285c4bb1518de3b08207918d001
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-09-24 18:38:15 +0000

    it removes logic to create logout button. Instead of create it , it uses the component

--- src/components/userbar/index.jsx
+++ src/components/userbar/index.jsx
@@ -1,27 +1,5 @@
-import { signOut, useSession } from 'next-auth/react';
-//components
-import { Button } from 'primereact/button';
 import { Menubar } from 'primereact/menubar';
-//hooks
-import request from '@services/api';
+import { LogOutButton } from '@components/logoutbutton';
 export const Userbar = (props) => {
-    const { data: session } = useSession();
-    const handleSignoutClick = async (event) => {
-        event.preventDefault();
-        const response = await request.apiAuthDelete(process.env.NEXT_PUBLIC_SIGNOUT, session);
-        if (response.ok) {
-            signOut({ callbackUrl: '/login' });
-        }
-    };
-    const RightElement = () => (
-        <Button
-            label="Cerrar sesion"
-            iconPos="left"
-            icon="pi pi-sign-out"
-            className="p-button-raised p-button-text p-button-plain"
-            onClick={handleSignoutClick}
-        />
-    );
-
-    return <Menubar model={[]} end={<RightElement />} />;
+    return <Menubar model={[]} end={<LogOutButton />} />;
 };
