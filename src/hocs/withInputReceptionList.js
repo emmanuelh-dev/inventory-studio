4d7500452068047e200091c21265c6c4bc2c4c21
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-09-27 23:45:11 +0000

    it adds logic to delete input document. If delete is completed successfuly then it shows green toast otherwise shows red toast notifications with error messahe

--- src/hocs/withInputReceptionList.js
+++ src/hocs/withInputReceptionList.js
@@ -1,8 +1,12 @@
-import { DOCUMENT_TYPES } from '@constants';
+import { MESSAGES } from '@messages';
 import services from '@services/api-services';
+import { useNotification } from '@hooks/useNotification';
+import { DOCUMENT_TYPES, MESSAGE_TYPES } from '@constants';
 import { documentSearchFields, documentFilters } from '@constants/options';
+
 export const withInputReceptionList = (WrappedComponent) => {
     const WithInputReceptionList = () => {
+        const { notification, showNotification } = useNotification();
         const getDataAsPage = async () => {
             const result = await services.findAllReceptionDocumentAsPage(DOCUMENT_TYPES.INPUT);
             return result;
@@ -24,13 +28,26 @@ export const withInputReceptionList = (WrappedComponent) => {
             return result;
         };
 
+        const removeDocument = async (document) => {
+            try {
+                await services.deleteReceptionDocument(document);
+                showNotification(MESSAGE_TYPES.SUCCESS, MESSAGES.SUCESS_RECORD_DELETED);
+                window.location.reload(false);
+            } catch (error) {
+                showNotification(MESSAGE_TYPES.ERROR, error.message);
+            }
+        };
+
         const listProps = {
+            notification,
+            remove: removeDocument,
             filters: documentFilters,
             fields: documentSearchFields,
             getDataAsPage: getDataAsPage,
             getDataByPage: getDataByPage,
             redirect: 'reception/INPUT/id/',
             getDataByFilter: getDataByFilter,
+            header: MESSAGES.DOCUMENT_DELETE_CONFIRMATION,
         };
 
         return <WrappedComponent {...listProps} />;
