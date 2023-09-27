a0c773011bfe16a15afd709f577ab78226d6bfec
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-09-27 23:49:39 +0000

    it adds logic to remove sales return document

--- src/hocs/withSalesReturnReceptionList.js
+++ src/hocs/withSalesReturnReceptionList.js
@@ -1,9 +1,13 @@
-import { DOCUMENT_TYPES } from '@constants';
+import { MESSAGES } from '@messages';
 import services from '@services/api-services';
+import { useNotification } from '@hooks/useNotification';
+import { DOCUMENT_TYPES, MESSAGE_TYPES } from '@constants';
 import { documentSearchFields, documentFilters } from '@constants/options';
 
 export const withSalesReturnReceptionList = (WrappedComponent) => {
     const WithSalesReturnReceptionList = () => {
+        const { notification, showNotification } = useNotification();
+
         const getDataAsPage = async () => {
             const result = await services.findAllReceptionDocumentAsPage(
                 DOCUMENT_TYPES.SALES_RETURN
@@ -27,13 +31,26 @@ export const withSalesReturnReceptionList = (WrappedComponent) => {
             return result;
         };
 
+        const removeDocument = async (document) => {
+            try {
+                await services.deleteReceptionDocument(document);
+                showNotification(MESSAGE_TYPES.SUCCESS, MESSAGES.SUCESS_RECORD_DELETED);
+                window.location.reload(false);
+            } catch (error) {
+                showNotification(MESSAGE_TYPES.ERROR, error.message);
+            }
+        };
+
         const listProps = {
+            notification,
+            remove: removeDocument,
             filters: documentFilters,
             fields: documentSearchFields,
             getDataAsPage: getDataAsPage,
             getDataByPage: getDataByPage,
-            redirect: 'reception/SALES_RETURN/id/',
             getDataByFilter: getDataByFilter,
+            redirect: 'reception/SALES_RETURN/id/',
+            header: MESSAGES.DOCUMENT_DELETE_CONFIRMATION,
         };
 
         return <WrappedComponent {...listProps} />;
