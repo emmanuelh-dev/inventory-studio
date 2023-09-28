dd8e282cd9c10eb6b7a6b246da22a2fd52d431da
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-09-28 02:14:49 +0000

    it adds logic to remove output document

--- src/hocs/withOutputDispatchList.js
+++ src/hocs/withOutputDispatchList.js
@@ -1,9 +1,12 @@
-import { DOCUMENT_TYPES } from '@constants';
+import { MESSAGES } from '@messages';
 import services from '@services/api-services';
+import { useNotification } from '@hooks/useNotification';
+import { DOCUMENT_TYPES, MESSAGE_TYPES } from '@constants';
 import { documentSearchFields, documentFilters } from '@constants/options';
 
 export const withOutputDispatchList = (WrappedComponent) => {
     const WithOutputDispatchList = () => {
+        const { notification, showNotification } = useNotification();
         const getDataAsPage = async () => {
             const result = await services.findAllDispatchDocumentAsPage(DOCUMENT_TYPES.OUTPUT);
             return result;
@@ -25,13 +28,25 @@ export const withOutputDispatchList = (WrappedComponent) => {
             return result;
         };
 
+        const removeDocument = async (document) => {
+            try {
+                await services.deleteDispatchDocument(document);
+                showNotification(MESSAGE_TYPES.SUCCESS, MESSAGES.SUCESS_RECORD_DELETED);
+            } catch (error) {
+                showNotification(MESSAGE_TYPES.ERROR, error.message);
+            }
+        };
+
         const listProps = {
+            notification,
+            remove: removeDocument,
             filters: documentFilters,
             fields: documentSearchFields,
             getDataAsPage: getDataAsPage,
             getDataByPage: getDataByPage,
             redirect: 'dispatch/OUTPUT/id/',
             getDataByFilter: getDataByFilter,
+            header: MESSAGES.DOCUMENT_DELETE_CONFIRMATION,
         };
 
         return <WrappedComponent {...listProps} />;
