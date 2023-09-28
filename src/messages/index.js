1be90a13201307eab638691f35ea0994cd3d9e79
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-09-28 05:31:09 +0000

    it changes message content for delete item and delete document

--- src/messages/index.js
+++ src/messages/index.js
@@ -1,7 +1,8 @@
 export const MESSAGES = {
-    DOCUMENT_DELETE_CONFIRMATION: 'Documento',
     CONTROL_TOTAL_AMOUNT: 'Control Monto Total',
+    ITEM_DELETE_CONFIRMATION: 'Articulo con nombre',
     CONTROL_TOTAL_QUANTITY: 'Control Cantidad Total',
+    DOCUMENT_DELETE_CONFIRMATION: 'Documento con folio',
     ERROR_RECORD_CREATED: 'El registro no fue guardado',
     SUCCESS_RECORD_CREATED: 'El registro creado con exito',
     SUCESS_RECORD_DELETED: 'El registro fue eliminado con exito',
