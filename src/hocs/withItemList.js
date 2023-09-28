b1f4412a9e1ea3e2060c36951b9a4ad404498d10
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-09-28 05:30:08 +0000

    it adds logic to remove item

--- src/hocs/withItemList.js
+++ src/hocs/withItemList.js
@@ -1,9 +1,13 @@
+import { MESSAGES } from '@messages';
+import { MESSAGE_TYPES } from '@constants';
 import services from '@services/api-services';
 import { ListToolbar } from '@components/listtoolbar';
+import { useNotification } from '@hooks/useNotification';
 import { itemSearchFields, itemFilters } from '@constants/options';
 
 export const withItemList = (WrappedComponent) => {
     const WithItemList = (props) => {
+        const { notification, showNotification } = useNotification();
         const getDataAsPage = async () => {
             const result = await services.findAllItemsAsPage();
             return result;
@@ -19,16 +23,29 @@ export const withItemList = (WrappedComponent) => {
             return result;
         };
 
+        const removeItem = async (item) => {
+            try {
+                await services.deleteItem(item.id);
+                showNotification(MESSAGE_TYPES.SUCCESS, MESSAGES.SUCESS_RECORD_DELETED);
+            } catch (error) {
+                showNotification(MESSAGE_TYPES.ERROR, error.message);
+            }
+        };
+
         const toolbar = <ListToolbar creationPath="item" />;
 
         const listProps = {
             toolbar,
+            notification,
             redirect: 'item/',
+            remove: removeItem,
             filters: itemFilters,
+            headerValue: 'itemName',
             fields: itemSearchFields,
             getDataAsPage: getDataAsPage,
             getDataByPage: getDataByPage,
             getDataByFilter: getDataByFilter,
+            header: MESSAGES.ITEM_DELETE_CONFIRMATION,
         };
 
         return <WrappedComponent {...listProps} />;
