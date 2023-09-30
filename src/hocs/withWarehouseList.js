3e2acf843ff63672be7cd45dc1c853d67595d66f
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-09-30 03:16:21 +0000

    it adds logic to delete warehouse from list page

--- src/hocs/withWarehouseList.js
+++ src/hocs/withWarehouseList.js
@@ -1,9 +1,13 @@
+import { MESSAGES } from '@messages';
+import { MESSAGE_TYPES } from '@constants';
 import services from '@services/api-services';
 import { ListToolbar } from '@components/listtoolbar';
+import { useNotification } from '@hooks/useNotification';
 import { warehouseSearchFields, warehouseFilters } from '@constants/options';
 
 export const withWarehouseList = (WrappedComponent) => {
     const WithWarehouseList = () => {
+        const { notification, showNotification } = useNotification();
         const getDataAsPage = async () => {
             const result = await services.findAllWarehousesAsPage();
             return result;
@@ -19,16 +23,29 @@ export const withWarehouseList = (WrappedComponent) => {
             return result;
         };
 
+        const removeWarehouse = async (warehouse) => {
+            try {
+                await services.deleteWarehouse(warehouse.id);
+                showNotification(MESSAGE_TYPES.SUCCESS, MESSAGES.SUCESS_RECORD_DELETED);
+            } catch (error) {
+                showNotification(MESSAGE_TYPES.ERROR, error.message);
+            }
+        };
+
         const toolbar = <ListToolbar creationPath="warehouse" />;
 
         const listProps = {
             toolbar,
+            notification,
             redirect: 'warehouse/',
+            remove: removeWarehouse,
             filters: warehouseFilters,
+            headerValue: 'warehouseName',
             fields: warehouseSearchFields,
             getDataAsPage: getDataAsPage,
             getDataByPage: getDataByPage,
             getDataByFilter: getDataByFilter,
+            header: MESSAGES.WAREHOUSE_DELETE_CONFIRMATION,
         };
 
         return <WrappedComponent {...listProps} />;
